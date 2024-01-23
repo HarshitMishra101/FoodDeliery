904e648fb006a8d8292da2420a9a152668d07061
Vaity <madurvaity2001@gmail.com>
2024-01-23 09:57:53 +0530

    changes in restaurant

--- src/main/java/com/example/demo/model/RestaurantList.java
+++ src/main/java/com/example/demo/model/RestaurantList.java
@@ -1,8 +1,17 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class RestaurantList {
-		
-	Integer RestaurantId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int restaurantId;
+	private String restaurantName;
+	private String restaurantLocation;
 	
 	
 	
